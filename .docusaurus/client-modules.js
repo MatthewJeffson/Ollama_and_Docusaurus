9739075b8767375a91d6053a91afbf86268e55eb
Matthew <104187183+MatthewJeffson@users.noreply.github.com>
2024-06-05 11:31:59 +0800

    update

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/jianfengyu/Desktop/We have a LONG LONG LONG trip/chat_Matthew/Ollama_and_Docusaurus/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/jianfengyu/Desktop/We have a LONG LONG LONG trip/chat_Matthew/Ollama_and_Docusaurus/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/jianfengyu/Desktop/We have a LONG LONG LONG trip/chat_Matthew/Ollama_and_Docusaurus/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/jianfengyu/Desktop/We have a LONG LONG LONG trip/chat_Matthew/Ollama_and_Docusaurus/src/css/custom.css"),
+];
